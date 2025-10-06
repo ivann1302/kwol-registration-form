dfa537058c42cb1e2b40bde70402d0d18f9b7424
ivan <ivann97n@gmail.com>
2025-10-06 21:11:13 +0300

    install react-router and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div />
-}
+export default function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <div className="p-6 rounded-lg shadow bg-rose-100">
+            <h1 className="text-3xl font-bold text-gray-800">Привет, Tailwind!</h1>
+          </div>
+        </div>
+    )
+  }
 
-export default App
+
